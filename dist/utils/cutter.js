2496bb92751f5a574e146c038b3991b624f8b329
Yu-Sheng Tzou <daniel.tzou1021@gmail.com>
2024-06-14 15:15:41 +0800

    Edited main.ts

---
+++ dist/utils/cutter.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+import { SUBTRACTION, Brush, Evaluator } from 'three-bvh-csg/build/index.module.js';
+export function sliceModel(mesh, curvePoints) {
+    const material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+    // Create the Brush from the mesh geometry
+    const meshBrush = new Brush(mesh.geometry);
+    // Create the shape and extrude it to create geometry
+    const curveShape = new THREE.Shape(curvePoints);
+    const curveGeometry = new THREE.ExtrudeGeometry(curveShape, { depth: 0.1, bevelEnabled: false });
+    // Create a THREE.Mesh for the curve geometry to update its properties
+    const curveMesh = new THREE.Mesh(curveGeometry, material);
+    curveMesh.position.y = 0.5;
+    curveMesh.updateMatrixWorld(true);
+    // Create the Brush from the updated curve geometry
+    const curveBrush = new Brush(curveMesh.geometry);
+    const evaluator = new Evaluator();
+    const slicedGeometryCSG = evaluator.evaluate(meshBrush, curveBrush, SUBTRACTION).geometry;
+    const slicedRegion = new THREE.Mesh(slicedGeometryCSG, material);
+    const remainingModel = new THREE.Mesh(mesh.geometry, material);
+    return [slicedRegion, remainingModel];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// 引入相關套件與副程式
 import * as THREE from 'three';
 import { onMouseClick } from './utils/onMouseClick';
 import { createCurve } from './utils/createCurve';
@@ -15,7 +16,10 @@ let sceneObjects = {
 };
 
 
+// 主程式
 function main() {
+
+    // 宣告場景變數
     const sceneParams = {
         canvasId: 'c',
         cameraPosition: new THREE.Vector3(0, 8, 10),
@@ -25,13 +29,21 @@ function main() {
             obj: '.././model/lulu/texturedMesh.obj'
         }
     };
+    // 曲線擬合按鈕
+    const fit_curve = document.getElementById('fit-curve') as HTMLButtonElement;
 
+
+    // ========================================
+    // 呼叫副程式
+    // ========================================
+
+
+    // 建立場景
     constructScene(sceneParams, sceneObjects);
-
+    // 視窗事件監聽，鼠標點擊
     window.addEventListener('click', (event) => onMouseClick(event, sceneObjects.scene, sceneObjects.camera, sceneObjects.raycaster, markedPoints));
-
-    const button = document.getElementById('fit-curve') as HTMLButtonElement;
-    button.addEventListener('click', () => {
+    // 視窗事件監聽，曲線擬合按鈕點擊
+    fit_curve.addEventListener('click', () => {
         if (markedPoints.length > 0) {
             const fittedCurvePoints = createCurve(markedPoints, sceneObjects.scene);
             if (sceneObjects.faceMesh) {
@@ -41,7 +53,9 @@ function main() {
             alert('Please mark some points first!');
         }
     });
+
 }
+
 
 main();
 
